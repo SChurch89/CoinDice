d5f7eb9bc61d8830fe040b32d85bd916a5694446
Zak <slocumz@ferris.edu>
2021-06-09 17:23:25 -0400

    Program simulates coin toss.

--- src/CoinToss.java
+++ src/CoinToss.java
@@ -1,4 +1,19 @@
+import java.util.Random;
 
 public class CoinToss {
-
+	
+	public static void main(String[] args) {
+		
+		if(coinToss() >= 0.5) {
+			System.out.println("Heads!");
+		} else {
+			System.out.println("Tails!");
+		}
+	}
+	
+	static double coinToss() {
+		Random toss = new Random(); 
+		double result = toss.nextDouble();
+		return result;
+	}
 }
