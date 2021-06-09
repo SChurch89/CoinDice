b7c9f09eeb6de09e214bac40876670918c34d6a2
Zak <slocumz@ferris.edu>
2021-06-09 17:27:21 -0400

    Program simulates multiple coin tosses.

--- src/CoinToss.java
+++ src/CoinToss.java
@@ -3,17 +3,23 @@ import java.util.Random;
 public class CoinToss {
 	
 	public static void main(String[] args) {
-		
-		if(coinToss() >= 0.5) {
-			System.out.println("Heads!");
-		} else {
-			System.out.println("Tails!");
-		}
+		multipleCoins();
 	}
 	
 	static double coinToss() {
 		Random toss = new Random(); 
 		double result = toss.nextDouble();
 		return result;
+	}
+	
+	static void multipleCoins() {
+		System.out.println("Testing 5 coin tosses.");
+		for (int i = 0; i < 5; i++) {
+			if(coinToss() >= 0.5) {
+				System.out.println("Heads!");
+			} else {
+				System.out.println("Tails!");
+			}
+		}
 	}
 }
